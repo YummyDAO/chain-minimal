30e7d75ed6ae7f9be2a4f1e45c64ab79d9ea61c9
YummyDAO <111630438+YummyDAO@users.noreply.github.com>
2024-06-14 22:16:10 +0100

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,10 +17,10 @@ $MINID_BIN config set client chain-id demo
 $MINID_BIN config set client keyring-backend test
 $MINID_BIN keys add alice
 $MINID_BIN keys add bob
-$MINID_BIN init test --chain-id demo --default-denom mini
+$MINID_BIN init test --chain-id demo --default-denom doge
 # update genesis
-$MINID_BIN genesis add-genesis-account alice 10000000mini --keyring-backend test
-$MINID_BIN genesis add-genesis-account bob 1000mini --keyring-backend test
+$MINID_BIN genesis add-genesis-account alice 1000000000doge --keyring-backend test
+$MINID_BIN genesis add-genesis-account bob 1000doge --keyring-backend test
 # create default validator
-$MINID_BIN genesis gentx alice 1000000mini --chain-id demo
+$MINID_BIN genesis gentx alice 1000000doge --chain-id demo
 $MINID_BIN genesis collect-gentxs
